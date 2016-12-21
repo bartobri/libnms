1b10a5e5f77a900716b12834c878ad48d938cc99
Brian Barto <bartobrian@gmail.com>
2016-12-20 19:06:44 -0500

    Replace nms_get_mask_char() function with a one-liner that does the same job.
    	modified:   src/libnms.c

--- src/libnms.c
+++ src/libnms.c
@@ -20,10 +20,51 @@
 #define JUMBLE_LOOP_SPEED    35    // miliseconds between each jumble
 #define REVEAL_LOOP_SPEED    100   // miliseconds (must be evenly divisible by 5)
 
-#define SPACE      32
-#define NEWLINE    10
-#define TAB        9
-#define TAB_SIZE   4
+#define SPACE                32
+#define NEWLINE              10
+#define TAB                  9
+#define TAB_SIZE             4
+
+#define MASK_CHAR_COUNT      218   // Total characters in maskCharTable[] array.
+
+// Character table representing the character set know as CP437 used by
+// the original IBM PC - https://en.wikipedia.org/wiki/Code_page_437
+static char *maskCharTable[] = {
+	"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "~",
+	".", "/", ":", ";", "<", "=", ">", "?", "[", "\\", "]", "_", "{", "}",
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"\xc3\x87", "\xc3\xbc", "\xc3\xa9", "\xc3\xa2", "\xc3\xa4", "\xc3\xa0",
+	"\xc3\xa5", "\xc3\xa7", "\xc3\xaa", "\xc3\xab", "\xc3\xa8", "\xc3\xaf",
+	"\xc3\xae", "\xc3\xac", "\xc3\x84", "\xc3\x85", "\xc3\x89", "\xc3\xa6",
+	"\xc3\x86", "\xc3\xb4", "\xc3\xb6", "\xc3\xb2", "\xc3\xbb", "\xc3\xb9",
+	"\xc3\xbf", "\xc3\x96", "\xc3\x9c", "\xc2\xa2", "\xc2\xa3", "\xc2\xa5",
+	"\xc6\x92", "\xc3\xa1", "\xc3\xad", "\xc3\xb3", "\xc3\xba", "\xc3\xb1",
+	"\xc3\x91", "\xc2\xaa", "\xc2\xba", "\xc2\xbf", "\xc2\xac", "\xc2\xbd",
+	"\xc2\xbc", "\xc2\xa1", "\xc2\xab", "\xc2\xbb", "\xce\xb1", "\xc3\x9f",
+	"\xce\x93", "\xcf\x80", "\xce\xa3", "\xcf\x83", "\xc2\xb5", "\xcf\x84",
+	"\xce\xa6", "\xce\x98", "\xce\xa9", "\xce\xb4", "\xcf\x86", "\xce\xb5",
+	"\xc2\xb1", "\xc3\xb7", "\xc2\xb0", "\xc2\xb7", "\xc2\xb2", "\xc2\xb6",
+	"\xe2\x8c\x90", "\xe2\x82\xa7", "\xe2\x96\x91", "\xe2\x96\x92",
+	"\xe2\x96\x93", "\xe2\x94\x82", "\xe2\x94\xa4", "\xe2\x95\xa1",
+	"\xe2\x95\xa2", "\xe2\x95\x96", "\xe2\x95\x95", "\xe2\x95\xa3",
+	"\xe2\x95\x91", "\xe2\x95\x97", "\xe2\x95\x9d", "\xe2\x95\x9c",
+	"\xe2\x95\x9b", "\xe2\x94\x90", "\xe2\x94\x94", "\xe2\x94\xb4",
+	"\xe2\x94\xac", "\xe2\x94\x9c", "\xe2\x94\x80", "\xe2\x94\xbc",
+	"\xe2\x95\x9e", "\xe2\x95\x9f", "\xe2\x95\x9a", "\xe2\x95\x94",
+	"\xe2\x95\xa9", "\xe2\x95\xa6", "\xe2\x95\xa0", "\xe2\x95\x90",
+	"\xe2\x95\xac", "\xe2\x95\xa7", "\xe2\x95\xa8", "\xe2\x95\xa4",
+	"\xe2\x95\xa7", "\xe2\x95\x99", "\xe2\x95\x98", "\xe2\x95\x92",
+	"\xe2\x95\x93", "\xe2\x95\xab", "\xe2\x95\xaa", "\xe2\x94\x98",
+	"\xe2\x94\x8c", "\xe2\x96\x88", "\xe2\x96\x84", "\xe2\x96\x8c",
+	"\xe2\x96\x90", "\xe2\x96\x80", "\xe2\x88\x9e", "\xe2\x88\xa9",
+	"\xe2\x89\xa1", "\xe2\x89\xa5", "\xe2\x89\xa4", "\xe2\x8c\xa0",
+	"\xe2\x8c\xa1", "\xe2\x89\x88", "\xe2\x88\x99", "\xe2\x88\x9a",
+	"\xe2\x81\xbf", "\xe2\x96\xa0"
+};
 
 // Window position structure, linked list. Keeps track of every
 // character's position on the terminal, as well as other attributes.
@@ -46,7 +87,6 @@ struct interval {
 
 
 // Function prototypes (internal)
-char *nms_get_mask_char(void);
 int nms_get_color_by_name(char *);
 int nms_mk_wcwidth(wchar_t);
 static int nms_bisearch(wchar_t, const struct interval *, int);
@@ -177,7 +217,7 @@ char nms_exec(NmsArgs *args) {
 				list_pointer->source[1] = '\0';
 			}
 
-			list_pointer->mask = nms_get_mask_char();
+			list_pointer->mask = maskCharTable[rand() % MASK_CHAR_COUNT];
 			list_pointer->row = y;
 			list_pointer->col = x;
 			list_pointer->s1_time = r_time > 1000 ? r_time_l : r_time;
@@ -201,7 +241,7 @@ char nms_exec(NmsArgs *args) {
 	while (list_pointer != NULL && list_pointer->row <= termSizeRows) {
 		mvaddstr(list_pointer->row, list_pointer->col, list_pointer->mask);
 		refresh();
-		list_pointer->mask = nms_get_mask_char();
+		list_pointer->mask = maskCharTable[rand() % MASK_CHAR_COUNT];
 		list_pointer = list_pointer->next;
 		usleep(TYPE_EFFECT_SPEED * 1000);
 	}
@@ -222,7 +262,7 @@ char nms_exec(NmsArgs *args) {
 		list_pointer = start;
 		while (list_pointer != NULL && list_pointer->row <= termSizeRows) {
 			mvaddstr(list_pointer->row, list_pointer->col, list_pointer->mask);
-			list_pointer->mask = nms_get_mask_char();
+			list_pointer->mask = maskCharTable[rand() % MASK_CHAR_COUNT];
 			list_pointer = list_pointer->next;
 		}
 		refresh();
@@ -242,12 +282,12 @@ char nms_exec(NmsArgs *args) {
 				loop = true;
 				list_pointer->s1_time -= REVEAL_LOOP_SPEED;
 				if (list_pointer->s1_time % s1_remask_time == 0) {
-					list_pointer->mask = nms_get_mask_char();
+					list_pointer->mask = maskCharTable[rand() % MASK_CHAR_COUNT];
 				}
 			} else if (list_pointer->s2_time > 0) {
 				loop = true;
 				list_pointer->s2_time -= REVEAL_LOOP_SPEED;
-				list_pointer->mask = nms_get_mask_char();
+				list_pointer->mask = maskCharTable[rand() % MASK_CHAR_COUNT];
 			} else {
 				list_pointer->mask = list_pointer->source;
 				attron(A_BOLD);
@@ -324,54 +364,6 @@ char nms_exec(NmsArgs *args) {
 	}
 
 	return ret;
-}
-
-/*
- * char *nms_get_mask_char(void)
- *
- * DESCR:
- * Returns a random character from the maskChars string. 
- *
- */
-char *nms_get_mask_char(void) {
-	char *maskChars[] = {
-		"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "~",
-		".", "/", ":", ";", "<", "=", ">", "?", "[", "\\", "]", "_", "{", "}",
-		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-		"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-		"\xc3\x87", "\xc3\xbc", "\xc3\xa9", "\xc3\xa2", "\xc3\xa4", "\xc3\xa0",
-		"\xc3\xa5", "\xc3\xa7", "\xc3\xaa", "\xc3\xab", "\xc3\xa8", "\xc3\xaf",
-		"\xc3\xae", "\xc3\xac", "\xc3\x84", "\xc3\x85", "\xc3\x89", "\xc3\xa6",
-		"\xc3\x86", "\xc3\xb4", "\xc3\xb6", "\xc3\xb2", "\xc3\xbb", "\xc3\xb9",
-		"\xc3\xbf", "\xc3\x96", "\xc3\x9c", "\xc2\xa2", "\xc2\xa3", "\xc2\xa5",
-		"\xc6\x92", "\xc3\xa1", "\xc3\xad", "\xc3\xb3", "\xc3\xba", "\xc3\xb1",
-		"\xc3\x91", "\xc2\xaa", "\xc2\xba", "\xc2\xbf", "\xc2\xac", "\xc2\xbd",
-		"\xc2\xbc", "\xc2\xa1", "\xc2\xab", "\xc2\xbb", "\xce\xb1", "\xc3\x9f",
-		"\xce\x93", "\xcf\x80", "\xce\xa3", "\xcf\x83", "\xc2\xb5", "\xcf\x84",
-		"\xce\xa6", "\xce\x98", "\xce\xa9", "\xce\xb4", "\xcf\x86", "\xce\xb5",
-		"\xc2\xb1", "\xc3\xb7", "\xc2\xb0", "\xc2\xb7", "\xc2\xb2", "\xc2\xb6",
-		"\xe2\x8c\x90", "\xe2\x82\xa7", "\xe2\x96\x91", "\xe2\x96\x92",
-		"\xe2\x96\x93", "\xe2\x94\x82", "\xe2\x94\xa4", "\xe2\x95\xa1",
-		"\xe2\x95\xa2", "\xe2\x95\x96", "\xe2\x95\x95", "\xe2\x95\xa3",
-		"\xe2\x95\x91", "\xe2\x95\x97", "\xe2\x95\x9d", "\xe2\x95\x9c",
-		"\xe2\x95\x9b", "\xe2\x94\x90", "\xe2\x94\x94", "\xe2\x94\xb4",
-		"\xe2\x94\xac", "\xe2\x94\x9c", "\xe2\x94\x80", "\xe2\x94\xbc",
-		"\xe2\x95\x9e", "\xe2\x95\x9f", "\xe2\x95\x9a", "\xe2\x95\x94",
-		"\xe2\x95\xa9", "\xe2\x95\xa6", "\xe2\x95\xa0", "\xe2\x95\x90",
-		"\xe2\x95\xac", "\xe2\x95\xa7", "\xe2\x95\xa8", "\xe2\x95\xa4",
-		"\xe2\x95\xa7", "\xe2\x95\x99", "\xe2\x95\x98", "\xe2\x95\x92",
-		"\xe2\x95\x93", "\xe2\x95\xab", "\xe2\x95\xaa", "\xe2\x94\x98",
-		"\xe2\x94\x8c", "\xe2\x96\x88", "\xe2\x96\x84", "\xe2\x96\x8c",
-		"\xe2\x96\x90", "\xe2\x96\x80", "\xe2\x88\x9e", "\xe2\x88\xa9",
-		"\xe2\x89\xa1", "\xe2\x89\xa5", "\xe2\x89\xa4", "\xe2\x8c\xa0",
-		"\xe2\x8c\xa1", "\xe2\x89\x88", "\xe2\x88\x99", "\xe2\x88\x9a",
-		"\xe2\x81\xbf", "\xe2\x96\xa0"
-	};
-
-	return maskChars[rand() % 218];
 }
 
 /*
