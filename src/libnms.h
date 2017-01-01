d09359ea205203bd44dba0ada7145c7a833192b5
Brian Barto <bartobrian@gmail.com>
2017-01-01 15:35:58 -0500

    Document libnms header file
    	modified:   src/libnms.h

--- src/libnms.h
+++ src/libnms.h
@@ -1,7 +1,14 @@
+/*
+ * Copyright (c) 2016 Brian Barto
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License. See LICENSE for more details.
+ */
+
 #ifndef LIBNMS_H
 #define LIBNMS_H 1
 
-// Display the characters stored in the display queue
+// Function prototypes
 char nms_exec(char *);
 void nms_set_foreground_color(char *);
 void nms_set_return_opts(char *);
