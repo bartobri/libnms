f2c7f5c7f052faab713bb322595216cb076450b1
Brian Barto <bartobrian@gmail.com>
2017-04-01 12:16:00 -0400

    Update copyright info
    	modified:   LICENSE
    	modified:   src/libnms.c
    	modified:   src/nms.h

--- src/libnms.c
+++ src/libnms.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Brian Barto
+ * Copyright (c) 2017 Brian Barto
  * 
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the MIT License. See LICENSE for more details.
